6cbe055df6804c0287c01672dd647912fdab89c1
root <root@192-168-1-106.tpgi.com.au>
2018-10-09 07:48:25 -0400

    ln python to python3 when installing

--- centos-deploy.sh
+++ centos-deploy.sh
@@ -5,20 +5,24 @@
 
 #install ius release
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
-yum update
-
+yum -y update
+yum -y install fontconfig freetype freetype-devel fontconfig-devel libstdc++
+yum -y install bzip2 
 ## install yum replace plugin
 yum install yum-plugin-replace
 
 ## install python 3
-yum install -y python36u python36u-libs python36u-devel python36u-pip
-
+yum install -y python36u python36u-libs python36u-devel python36u-pip python36u-devel python36u-gunicorn python36u-lxml python36u-setproctitle python36u-setuptools python36u-test python36u-tkinter python36u-tools 
 ## install git
 yum -y install git2u-all
 
 ## install node js
 curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
 yum -y install nodejs
+
+# temporarily link to python3
+rm /usr/bin/python
+ln -s /usr/bin/python3.6 /usr/bin/python
 
 ln -s /usr/bin/pip3.6 /usr/bin/pip3
 ln -s /usr/bin/python3.6 /usr/bin/python3
@@ -29,4 +33,5 @@ pip3 install pyinvoke
 #=============================================================
 python3 deploy.py
 
-
+rm /usr/bin/python
+ln -s /usr/bin/python2 /usr/bin/python
